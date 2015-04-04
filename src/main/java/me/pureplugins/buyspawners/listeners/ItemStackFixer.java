93441cdc6f14f9bab1b0d6f30972245cdfc2f9b1
vemacs <gentoo@outlook.com>
2015-04-04 08:11:41 -0600

    use reflection to properly hijack the event

--- src/main/java/me/pureplugins/buyspawners/listeners/ItemStackFixer.java
+++ src/main/java/me/pureplugins/buyspawners/listeners/ItemStackFixer.java
@@ -8,12 +8,28 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.lang.reflect.Field;
+
 public class ItemStackFixer implements Listener {
+    private Field itemInHandField;
+
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockPlace(BlockPlaceEvent event) {
         ItemStack is = event.getItemInHand();
         if (is != null && is.getType() == Material.MOB_SPAWNER) {
-            event.getPlayer().setItemInHand(CreateSpawner.fixStack(is));
+            ItemStack fixed = CreateSpawner.fixStack(is);
+            if (fixed != is) {
+                try {
+                    if (itemInHandField == null) {
+                        itemInHandField = event.getClass().getDeclaredField("itemInHand");
+                        itemInHandField.setAccessible(true);
+                    }
+                    itemInHandField.set(event, fixed);
+                    event.getPlayer().setItemInHand(fixed);
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
