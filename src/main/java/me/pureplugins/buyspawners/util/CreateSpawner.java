8d5e7e3d406c2fc57027ea527fe764d61d2f35e4
vemacs <gentoo@outlook.com>
2015-04-05 17:54:47 -0600

    just log it

--- src/main/java/me/pureplugins/buyspawners/util/CreateSpawner.java
+++ src/main/java/me/pureplugins/buyspawners/util/CreateSpawner.java
@@ -2,7 +2,6 @@ package me.pureplugins.buyspawners.util;
 
 import de.dustplanet.util.SilkUtil;
 import me.pureplugins.buyspawners.Main;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
 
@@ -31,24 +30,7 @@ public class CreateSpawner {
                     }
                 }
             }
-            if (is.hasItemMeta()) {
-                String name = is.getItemMeta().getDisplayName();
-                if (name != null && !name.isEmpty() && !ChatColor.stripColor(name).equals(name)) {
-                    Main.getInstance().getLogger().warning("Falling back to displayname for " + name);
-                    name = ChatColor.stripColor(name.toLowerCase());
-                    String[] nameParts = name.split(" ");
-                    for (String part : nameParts) {
-                        if (part.equalsIgnoreCase("spawner")) {
-                            continue;
-                        }
-                        if (su.isKnown(part)) {
-                            short entityID = su.name2Eid.get(part);
-                            return su.newSpawnerItem(entityID, su.getCustomSpawnerName(su.eid2MobID.get(entityID)),
-                                    is.getAmount(), false);
-                        }
-                    }
-                }
-            }
+            Main.getInstance().getLogger().warning("Corrupted NBT entityId detected: " + vanillaNbtId);
         }
         return is;
     }
